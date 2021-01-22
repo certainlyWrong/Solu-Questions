fa50051f7cfe970ae5319043c4c2b667ab260abd
Adriano R. de Sousa <adriano42335@gmail.com>
2021-01-22 16:00:39 -0300

    sgunda atualizacao

---
+++ 1070.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 0, i = 0;
+    scanf("%d", &x);
+    while (1)
+    {
+        if (x % 2 != 0)
+        {
+            printf("%d\n", x), i++;
+            if(i >= 6) break;
+        }
+        x++;
+    }
+    return 0;
+}
