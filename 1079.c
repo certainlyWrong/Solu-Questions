0139cc825903e5a548ee976b356de5efc3d76ece
Adriano R. de Sousa <adriano42335@gmail.com>
2021-02-01 15:25:05 -0300

    mais um dia

---
+++ 1079.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 0;
+    scanf("%d", &n);
+
+    while (n)
+    {
+        double a = 0, b = 0, c = 0;
+        scanf("%lf %lf %lf", &a, &b, &c);
+        printf("%.1lf\n", (a * 2 + b * 3 + c * 5)/10);
+        n--;
+    }
+
+    return 0;
+}
