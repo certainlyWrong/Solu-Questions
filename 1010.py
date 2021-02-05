e3e1d967f23ba1dcc3da171404e4be1302e22a3b
certainlyWrong <adriano42335@gmail.com>
2021-02-05 13:49:49 -0300

    att python3.8

---
+++ 1010.py
@@ -0,0 +1,5 @@
+#!python3
+dados1 = list([float(x) for x in input().split()])
+dados2 = list([float(x) for x in input().split()])
+
+print('VALOR A PAGAR: R$ %.2f' %(dados1[1] * dados1[2] + dados2[1] * dados2[2]))
