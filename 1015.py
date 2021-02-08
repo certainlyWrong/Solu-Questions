28a29d6cf874d301032216121988a70b312f9488
certainlyWrong <adriano42335@gmail.com>
2021-02-07 23:52:13 -0300

    mais um dia

---
+++ 1015.py
@@ -0,0 +1,12 @@
+
+from math import sqrt
+
+pontos = [[float(x) for x in input().split()], [float(x) for x in input().split()]]
+
+resultado = sqrt(pow(pontos[1][1] - pontos[0][1], 2) + pow(pontos[1][0] - pontos[0][0], 2))
+
+# python3.8
+# print(f'{resultado:.4f}')
+
+# python3.4
+print('{:.4f}' .format(resultado))
---
+++ 1036.py
@@ -0,0 +1,18 @@
+from math import pow, sqrt
+
+values = [float(x) for x in input().split()]
+
+delta = pow(values[1], 2) - 4 * values[0] * values[2]
+
+if delta < 0 or values[0] == 0:
+    print('Impossivel calcular')
+
+else:
+    roots = [
+        ((-values[1]) + sqrt(delta))/(2 * values[0]),
+        ((-values[1]) - sqrt(delta))/(2 * values[0])
+    ]
+    # python3.8
+    # print(f'R1 = {roots[0]:.5f}\nR2 = {roots[1]:.5f}')
+    # python3.4
+    print('R1 = {:.5f}\nR2 = {:.5f}' .format(roots[0], roots[1]))
