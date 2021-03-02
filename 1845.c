0011f26f7615d6cbe7ca57591f6bfaf449dd2d84
adriano r. de sousa <adrianorodriguesdsousa@ufpi.edu.br>
2021-03-01 21:44:02 -0300

    mais um dia

--- 1845.c
+++ 1845.c
@@ -6,38 +6,48 @@ int main(void)
     char text, conso[] = {"bjpsvxzBJPSVXZ"};
     int i, j;
     bool antF = false;
-
+    
     while(scanf("%c", &text) != EOF)
     {
-        bool print_caracter = true;
-
+        
         if (text != 'f' && text != 'F')
         {
-            for ( i = 0; i < 14 && print_caracter == true; i++)
+            bool print_caracter = true;
+            
+            if (text != 255)
             {
-                if (text == conso[i] && i < 7)
+                if ((text >= 65 && text <= 90) || (text >= 97 && 122)) 
                 {
-                    if (antF == true)
+                    for ( i = 0; i < 14 && print_caracter == true; i++)
                     {
-                        print_caracter = false;
-                        break;
+                        if (text == conso[i] && i < 7)
+                        {
+                            if (antF == true)
+                            {
+                                print_caracter = false;
+                                break;
+                            }
+                            printf("f");
+                            antF = true;
+                            print_caracter = false;
+                        }
+                        else
+                        if (text == conso[i] && i < 14)
+                        {
+                            if (antF == true)
+                            {
+                                print_caracter = false;
+                                break;
+                            }
+                            printf("F");
+                            antF = true;
+                            print_caracter = false;
+                        }
+                    
                     }
-                    printf("f");
-                    antF = true;
-                    print_caracter = false;
-                }
-                else if (text == conso[i] && i < 14)
-                {
-                    if (antF == true)
-                    {
-                        print_caracter = false;
-                        break;
-                    }
-                    printf("F");
-                    antF = true;
-                    print_caracter = false;
                 }
             }
+
             if (print_caracter == true)
             {
                 printf("%c", text);
