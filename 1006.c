f0fb5dec15045370d13698480924ef745494a316
adriano r. de sousa <adrianorodriguesdsousa@ufpi.edu.br>
2021-03-23 23:30:14 -0300

    mais um dia

--- 1006.c
+++ 1006.c
@@ -1,7 +1,8 @@
 
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
     double n1, n2, n3;
     scanf("%lf %lf %lf", &n1, &n2, &n3);
     printf("MEDIA = %.1lf\n", (n1 * 2 + n2 * 3 + n3 * 5)/10);
