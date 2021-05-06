df77c1497a758e05ec81d693555495b3b9c8c9aa
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2021-05-06 11:43:33 -0300

    mais um dia

--- 1036.c
+++ 1036.c
@@ -1,16 +1,36 @@
-
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-int main(){
-    double a = 0, b = 0, c = 0, delta = 0;
-    scanf("%lf %lf %lf", &a, &b, &c);
-    delta = b*b - 4 * a * c;
-    if (a == 0 || delta < 0)
+typedef struct 
+{
+    float x1, x2;
+} Raiz;
+
+Raiz calculaRaiz(float, float, float);
+
+int main(void)
+{
+    float a, b, c;
+    scanf("%f %f %f", &a, &b, &c);
+
+    float delta = b*b - 4 * a * c;
+    if (delta < 0 || a == 0)
+    {
         printf("Impossivel calcular\n");
-    else{
-        delta = sqrt(delta);
-        printf("R1 = %.5lf\nR2 = %.5lf\n", ((-b) + delta)/(2*a), ((-b) - delta)/(2*a));
+        return 0;
     }
+    
+    Raiz resultado = calculaRaiz(a, b, c);
+
+    printf("R1 = %.5f\nR2 = %.5f\n", resultado.x1, resultado.x2);
+
     return 0;
+}
+
+Raiz calculaRaiz(float a, float b, float c)
+{
+    float delta = b*b - 4 * a * c;
+    Raiz retorno = {(-b + sqrt(delta))/(a*2),
+                    (-b - sqrt(delta))/(a*2)};
+    return retorno;
 }
--- 2162.c
+++ 2162.c
@@ -1,32 +1,40 @@
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
-int main(void){
-	int medidas = 0, i = 0;
-	bool true_false = true;
-	scanf("%d", &medidas);
-	int array[medidas];
-	for (i = 0; i < medidas; i++)
-		scanf("%d", &array[i]);
-	if(medidas > 1){
-		if (medidas == 2 && array[0] == array[1])
-			true_false = false;
+int tipoPaisagem(int *, int);
+
+int main(void)
+{
+    int tam, i;
+    scanf("%d", &tam);
+    int array[tam];
+    for (i = 0; i < tam; i++)
+        scanf("%d", &array[i]);
+
+    printf("%d\n", tipoPaisagem(array, tam));
+    
+    return 0;
+}
+
+int tipoPaisagem(int padrao[], int tamanho)
+{
+    int true_false = 1, i;
+    if(tamanho > 1){
+		if (tamanho == 2 && padrao[0] == padrao[1])
+			true_false = 0;
 		else{
-			for (i = 1; i < medidas - 1; i++){
-				if (array[i - 1] >= array[i] && array[i] >= array[i + 1]){
-					true_false = false;
+			for (i = 1; i < tamanho - 1; i++){
+				if (padrao[i - 1] >= padrao[i] && padrao[i] >= padrao[i + 1]){
+					true_false = 0;
 					break;
 				}
-				else if (array[i - 1] <= array[i] && array[i] <= array[i + 1]){
-					true_false = false;
+				else if (padrao[i - 1] <= padrao[i] && padrao[i] <= padrao[i + 1]){
+					true_false = 0;
 					break;
 				}
 			}
 		}
 	}
-	if (true_false == true) printf("%d\n", 1);
-	else printf("%d\n", 0);
-	return 0;
+    if (true_false == 1)
+        return 1;
+    return 0;
 }
